30de0c58cdf4df06c11e74a57a78b9b4d771d6ad
fastcamedu <fastcamedu@gmail.com>
2023-02-16 11:14:20 +0900

    Init Spring Batch Sample Project

---
+++ README.md
@@ -0,0 +1,18 @@
+# 저장소 설명
+Spring Batch를 활용한 배치 처리 프로그램 샘플
+
+## 강의 정보
+| 분류    | 강의명                          |
+|-------|------------------------------|
+| 강의명   | 초간단 이커머스 서비스에서 대규모 서비스로 도약하기 |
+| 파트 2  | 스타트업 성장 단계                   |
+| 챕터 08 | 초간단 배치 서비스 만들기               |
+| 클립 04 | 스프링 배치 실행하기                  |
+
+### 필수 의존성
+```groovy
+dependencies {
+    implementation 'org.springframework.boot:spring-boot-starter-batch'
+    implementation 'org.springframework.boot:spring-boot-starter-jdbc'
+}
+```
---
+++ src/main/java/com/fastcampus/hellospringbatch/HelloSpringBatchApplication.java
@@ -0,0 +1,17 @@
+package com.fastcampus.hellospringbatch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class HelloSpringBatchApplication {
+	public static void main(String[] args) {
+		System.exit(
+				SpringApplication.exit(
+						SpringApplication.run(HelloSpringBatchApplication.class, args)
+				)
+		);
+	}
+}
